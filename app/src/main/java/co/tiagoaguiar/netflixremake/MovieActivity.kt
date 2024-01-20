e1330c1b0ed08c851a31aa438421dc06f10409fb
Felipe Borba <felipe.borba0109@gmail.com>
2024-01-20 18:36:08 -0300

    add MovieActivity

---
+++ app/src/main/java/co/tiagoaguiar/netflixremake/MovieActivity.kt
@@ -0,0 +1,27 @@
+package co.tiagoaguiar.netflixremake
+
+import android.graphics.drawable.LayerDrawable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.widget.Toolbar
+import androidx.core.content.ContextCompat
+
+class MovieActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie)
+
+        val toolbar: Toolbar = findViewById(R.id.movie_toolbar)
+        setSupportActionBar(toolbar)
+
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.baseline_arrow_back_24)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        val layerDrawable = ContextCompat.getDrawable(this, R.drawable.shadows) as LayerDrawable
+        val movieCover = ContextCompat.getDrawable(this, R.drawable.movie_4)
+        layerDrawable.setDrawableByLayerId(R.id.cover_drawable, movieCover)
+        val coverImg: ImageView = findViewById(R.id.movie_img)
+        coverImg.setImageDrawable(layerDrawable)
+    }
+}
